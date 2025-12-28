0b602994f360e191d9233a34f5538e5e0bf917ce
chaladi tulasi ram <2200090214@kluniversity.in>
2025-12-28 09:41:35 +0530

    final3

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"; // <--- Did you add this import?
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // <--- Add this line
+    tailwindcss(), // <--- Did you add this function call?
   ],
   resolve: {
     alias: {
