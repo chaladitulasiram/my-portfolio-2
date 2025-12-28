efa3dbcde0e36fef66aecfc2080e82e9c96d18eb
chaladi tulasi ram <2200090214@kluniversity.in>
2025-12-28 09:25:34 +0530

    final2

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import { componentTagger } from "vite-plugin-component-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // <--- Add this line
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
